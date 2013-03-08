7aa0bd12fc5047fa7d681bc66d33501f69edfa45
Thnan <Thnan@localhost>
2013-03-08 11:43:10 -0500

    moved license to subfolder. added OptionalDeps to TOC. added method stubs for implementation later.

--- LibModuleDBShare-1.0/LibModuleDBShare-1.0.lua
+++ LibModuleDBShare-1.0/LibModuleDBShare-1.0.lua
@@ -2,3 +2,13 @@ local MAJOR, MINOR = "LibModuleDBShare-1.0", 1
 local LibModuleDBShare, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 
 if not LibModuleDBShare then return end -- No upgrade needed
+
+LibModuleDBShare.groups = LibModuleDBShare.groups or {};
+
+function LibModuleDBShare:NewGroup(groupName, usesDualSpec)
+	
+end
+
+function LibModuleDBShare:AddModule(groupName, db)
+
+end

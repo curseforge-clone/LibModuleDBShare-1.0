ffa50bab9fb0d4dd577e498e50125c741ca483f5
Thnan <Thnan@localhost>
2013-03-09 02:26:13 -0500

    added .docmeta file. added luadoc to LibModuleDBShare-1.0.lua. completed initial function stubs. back to auto change log.

--- LibModuleDBShare-1.0/LibModuleDBShare-1.0.lua
+++ LibModuleDBShare-1.0/LibModuleDBShare-1.0.lua
@@ -1,3 +1,10 @@
+--- **LibModuleDBShare-1.0**\\ 
+-- A description will eventually be here.
+--
+-- @usage
+-- Also coming soon.
+-- @class file
+-- @name LibModuleDBShare-1.0.lua
 local MAJOR, MINOR = "LibModuleDBShare-1.0", 1
 local LibModuleDBShare, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 
@@ -5,10 +12,31 @@ if not LibModuleDBShare then return end -- No upgrade needed
 
 LibModuleDBShare.groups = LibModuleDBShare.groups or {};
 
+local DBGroup = {};
+
+--- Creates a new DB group.
+-- @param groupName The name of the new DB group.
+-- @param usesDualSpec True if this group should use LibDualSpec, false otherwise.
+-- @usage
+-- local myAddonDBGroup = LibStub("LibModuleDBShare-1.0"):NewGroup("MyAddonGroupName", true)
+-- @return the new DB group object
 function LibModuleDBShare:NewGroup(groupName, usesDualSpec)
 	
 end
 
-function LibModuleDBShare:AddModule(groupName, db)
+--- Retrieves an existing DB group.
+-- @param groupName The name of the DB group to retrieve.
+-- @usage
+-- local myAddonDBGroup = LibStub("LibModuleDBShare-1.0"):GetGroup("MyAddonGroupName")
+-- @return the DB group object, or nil if not found
+function LibModuleDBShare:GetGroup(groupName)
+
+end
+
+--- Adds a database to the group.
+-- @param db The name of the new DB group.
+-- @usage
+-- myAddonDBGroup:AddDB(MyAddon.db)
+function DBGroup:AddDB(db)
 
 end

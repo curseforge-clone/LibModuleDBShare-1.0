4f8ef19fc96b5df8b1ed5d6a42560a84dc708a2c
Thnan < @localhost>
2013-03-10 03:34:49 -0400

    began implementation of NewGroup implemented GetGroup

--- LibModuleDBShare-1.0/LibModuleDBShare-1.0.lua
+++ LibModuleDBShare-1.0/LibModuleDBShare-1.0.lua
@@ -10,19 +10,29 @@ local LibModuleDBShare, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 
 if not LibModuleDBShare then return end -- No upgrade needed
 
+local AceDB = LibStub("AceDB-3.0");
+
 LibModuleDBShare.groups = LibModuleDBShare.groups or {};
 
 local DBGroup = {};
 
 --- Creates a new DB group.
 -- @param groupName The name of the new DB group.
--- @param usesDualSpec True if this group should use LibDualSpec, false otherwise.
+-- @param usesDualSpec True if this group should use LibDualSpec, false otherwise. (NYI)
 -- @param initialProfile The name of the profile to start with.
 -- @usage
 -- local myAddonDBGroup = LibStub("LibModuleDBShare-1.0"):NewGroup("MyAddonGroupName", true)
 -- @return the new DB group object
 function LibModuleDBShare:NewGroup(groupName, usesDualSpec, initialProfile)
-	
+	-- param checks go here
+	local group = {}
+	group.name = groupName;
+	group.syncDBTable = {};
+	group.syncDB = AceDB:New(group.syncDBTable, nil, true);
+	for k, v in pairs(DBGroup) do
+		group[k] = v;
+	end
+	return group;
 end
 
 --- Retrieves an existing DB group.
@@ -31,7 +41,7 @@ end
 -- local myAddonDBGroup = LibStub("LibModuleDBShare-1.0"):GetGroup("MyAddonGroupName")
 -- @return the DB group object, or nil if not found
 function LibModuleDBShare:GetGroup(groupName)
-
+	return LibModuleDBShare.groups[groupName];
 end
 
 --- Adds a database to the group.
